bcacc34da751e4092f201da21b0308ce388d898f
TomasFiori98 <1998tf@gmail.com>
2020-11-11 20:59:09 -0300

    Creacion de clases

---
+++ AdminReservasHotel/AdminReservasHotel/Model/Reserva.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminReservasHotel.Model
+{
+    class Reserva
+    {
+        private string id;
+        private DateTime fecha_ingreso;
+        private DateTime fecha_salida;
+        private Habitacion habitacion;
+        private Huesped titular_reserva;
+        private int cant_personas;
+        private bool pagado;
+
+        public Reserva(string id, DateTime fecha_ingreso, DateTime fecha_salida, int cant_personas, bool pagado)
+        {
+            this.id = id;
+            this.fecha_ingreso = fecha_ingreso;
+            this.fecha_salida = fecha_salida;
+            this.cant_personas = cant_personas;
+            this.pagado = pagado;
+        }
+
+        public void agregarHabitacion(Habitacion hab)
+        {
+            this.habitacion = hab;
+        }
+        public void asociarHuesped(Huesped hu)
+        {
+            this.titular_reserva = hu;
+        }
+
+        public void mostrarDatos()
+        {
+
+        }
+
+
+    }
+}
---
+++ AdminReservasHotel/AdminReservasHotel/Model/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminReservasHotel.Model
+{
+    abstract class Usuario
+    {
+        protected string nombre;
+        protected string apellido;
+        protected string correo;
+        protected string dni;
+        protected DateTime fecha_nacimiento;
+
+        public Usuario(string nombre, string apellido, string correo, string dni, DateTime fecha_nacimiento)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.correo = correo;
+            this.dni = dni;
+            this.fecha_nacimiento = fecha_nacimiento;
+        }
+
+        public Usuario()
+        {
+        }
+
+        public abstract void mostrarDatos();
+        public abstract void consultarReserva(Reserva r);
+        public abstract void consultarDisonibilidad();
+
+    }
+}
